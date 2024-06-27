ca011198a8b4d1d1ab09f9a7067e46cddbe3d508
Jose Perez <ihoseba@gmail.com>
2024-06-27 13:38:46 +0200

    actualizacion desarrollo

--- Ejercicios/5_es_palidromo_jose.py
+++ Ejercicios/5_es_palidromo_jose.py
@@ -1,6 +1,7 @@
 # Escribe una función que verifique si una cadena es un palíndromo.
 def es_palindromo(cadena):
     return cadena==cadena[::-1] 
+# modificamos desarrollo
 
 # Prueba la función con al menos 3 cadenas distintas
 cadenas=[
