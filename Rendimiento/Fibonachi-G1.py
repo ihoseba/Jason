2f8c8ad8d4b142fc0ded93c50e02e2540820a661
ihoseba <ihoseba@gmail.com>
2024-07-08 12:01:27 +0200

    update

--- Rendimiento/Fibonachi-G1.py
+++ Rendimiento/Fibonachi-G1.py
@@ -11,16 +11,25 @@ def fib(n):
         j = ((phi**n - (1 - phi)**n) / sqrt(5))
         return round(j)
 
-# Ejemplo
-n=int(input("Cual es n-esimo? "))
+mucho=False
+n=0
+resultado=00
+while mucho is False and resultado<10**300:
+    n += 1
 
-# Inicia el contador
-inicio = time.time()
+    # Inicia el contador
+    inicio = time.time()
 
-resultado = fib(n)
-print(f"El término cuando n = {n} es: {resultado}")
+    resultado = fib(n)
 
-# Detiene el contador e imprime el resultado
-fin = time.time()
-tiempo_transcurrido = fin - inicio
-print(f"El tiempo de ejecución es: {tiempo_transcurrido:.6f} segundos")
+    # Detiene el contador e imprime el resultado
+    fin = time.time()
+    tiempo_transcurrido = fin - inicio
+
+    if tiempo_transcurrido > 10:
+        mucho=True
+
+
+print(f"Iteracion: {n}")
+print(f"El tiempo de ejecución es: {tiempo_transcurrido:.1f} segundos")
+print(f"El término cuando n = {n} es:", resultado)
