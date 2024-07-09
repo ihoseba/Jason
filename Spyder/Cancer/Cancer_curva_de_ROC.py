ffea5d7f1e4b669ff90e1b7b1fae93360faf7f6d
ihoseba <ihoseba@gmail.com>
2024-07-09 12:31:14 +0200

    update

--- Spyder/Cancer/Cancer_curva_de_ROC.py
+++ Spyder/Cancer/Cancer_curva_de_ROC.py
@@ -16,10 +16,11 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 # Cargamos los datos y dividimos en conjunto de entrenamiento y prueba
 
 cancer = load_breast_cancer()
-print(type(cancer))
 
 X_train, X_test, y_train, y_test = train_test_split(
     cancer.data,
@@ -60,14 +61,28 @@ plt.ylabel('Sensibilidad / TPR')
 plt.show()
 
 
+# Predecimos las etiquetas de X_test (dataset de testing)
+prediccion_test = knn.predict(X_test)
 
+# Comparar exactitud de resultados
+exactitud = np.mean((prediccion_test == y_test))
+print("La exactitud del modelo es:", exactitud)
 
+exactitud_knn = knn.score(X_test, y_test)
+print("La exactitud del modelo es (método 2):", exactitud_knn)
 
+# Obtenemos matriz de confusión
 
+mat_conf = confusion_matrix(y_true=y_test, y_pred=prediction_test)
 
-
-
-
-
+# Trazado de la matriz de confusión
+fig, ax = plt.subplots(figsize=(2.5,2.5))
+ax.matshow(mat_conf, cmap=plt.cm.Blues, alpha=.3)
+for i in range(mat_conf.shape[0]):
+    for j in range(mat_conf.shape[1]):
+        val = mat_conf[i, j]
+        ax.text(x=j, y=i, s=val, va='center', ha='center')
+        plt.xlabel("Valores predichos")
+        plt.ylabel("Valores reales")
 
 
