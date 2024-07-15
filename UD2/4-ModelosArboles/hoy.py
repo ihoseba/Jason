189bad0a51c78e6cfec18ddc7a18f97379506d06
ihoseba <ihoseba@gmail.com>
2024-07-15 10:32:05 +0200

    Cambiamos estructura directorios

---
+++ UD2/4-ModelosArboles/hoy.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 10:16:12 2024
+
+@author: joseangelperez
+"""
+
--- UD2/4-ModelosArboles/iris_decision_tree - Jose.py
+++ UD2/4-ModelosArboles/iris_decision_tree - Jose.py
@@ -24,18 +24,29 @@ from sklearn.tree import export_graphviz
 
 from pydotplus import graph_from_dot_data
 
-#Cargamos en X la longitud y anchura del pétalo, tal y como hemos hecho en secciones anteriores
+# para el entrenamiento del algoritmo, un subconjunto de los elementos a clasificar
+# restantes se pueden utilizar para las pruebas.
+# En este caso, te animo a que utilices los procedimientos que ya conoces para
+
+# obtener los datos de entrenamiento del conjunto iris y
+
+#Cargamos en X la longitud y anchura del pétalo, tal y como hemos hecho en
+# secciones anteriores
 #En y se vuelca la clasificación de cada lirio (0, 1, 2)
 iris=load_iris()
 X=iris.data[:,2:]
 y=iris.target
 
-#Creamos un objeto arbol_clf con todos los atributos asociados a la construcción del DecisionTreeClassifier
-arbol_clf=DecisionTreeClassifier(max_depth=2)
+# que generes el árbol sin fijar un nivel máximo de profundidad, aplicando Gini.
+
+#Creamos un objeto arbol_clf con todos los atributos asociados a la construcción
+# del DecisionTreeClassifier
+arbol_clf=DecisionTreeClassifier(criterion="gini")
 arbol_clf.fit(X,y)
 
-export_graphviz(arbol_clf,out_file="arbol_iris.dot",feature_names=iris.feature_names[2:],
-class_names=iris.target_names,rounded=True,filled=True)
+export_graphviz(arbol_clf,out_file="arbol_iris.dot",
+                feature_names=iris.feature_names[2:],
+                class_names=iris.target_names,rounded=True,filled=True)
 
 arbol_data = export_graphviz(arbol_clf,out_file=None,feature_names=iris.feature_names[2:], class_names=iris.target_names,rounded=True,filled=True)
 
@@ -44,11 +55,7 @@ grafica = graph_from_dot_data(arbol_data)
 grafica.write_png("arbol.png")
 
 
-# para el entrenamiento del algoritmo, un subconjunto de los elementos a clasificar
-# restantes se pueden utilizar para las pruebas.
-# En este caso, te animo a que utilices los procedimientos que ya conoces para
-# obtener los datos de entrenamiento del conjunto iris y
-# que generes el árbol sin fijar un nivel máximo de profundidad, aplicando Gini.
+
 # Escribe el código necesario y obtén la gráfica de dicho árbol. 
 # ¿Qué profundidad final se alcanza y cómo son las últimas hojas?
 # Responde a la cuestión expuesta y, posteriormente, comprueba tu respuesta.
